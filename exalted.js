746a32eda224aed5ac423a207a9ebcc94ca30522
caileth <jcaletges@gmail.com>
2014-07-19 18:56:20 -0700

    basic die functionality

---
+++ exalted.js
@@ -0,0 +1,28 @@
+$(function() {
+	var DEFAULT_NUM_DICE = 5,
+		DEFAULT_DIE_SIDE = 10,
+		rollButton = $("#roll"),
+		numDice = $("#numDice");
+
+	$(rollButton).click(function() {
+		diceRoller(numDice, 10);console.log("Dice roller clicked");
+	});
+
+	function diceRoller(numDice, sides) {
+		if (!numDice) numDice = DEFAULT_NUM_DICE;
+		if (!sides) sides = DEFAULT_DIE_SIDE;
+		console.log("Rolling",numDice,sides,"sided dice");
+
+		for (var i = 0; i < numDice; i++) {
+			dieRoller(sides);
+		}
+	}
+
+	function dieRoller(sides) {
+		if (!sides) sides = DEFAULT_DIE_SIDE;
+		console.log("Rolling a %i-sided die",sides);
+
+		var result = Math.floor((Math.random() * sides) + 1);console.log("Rolled a",result);
+		return result;
+	}
+});
